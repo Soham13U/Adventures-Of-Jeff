381078d94c14304f151d6676598bca9731cd01b2
Soham <sohamup13@gmail.com>
2022-02-22 23:12:02 +0530

    Done till player attack

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState{
+    walk,
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {   public float speed = 4f;
     private Rigidbody2D myRigidBody;
     private Vector3 change;
     private Animator animator;
+    private PlayerState currentState;
     // Start is called before the first frame update
     void Start()
     {   
         animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
+        currentState = PlayerState.walk;
+        animator.SetFloat("moveX",0);
+        animator.SetFloat("moveY",-1);
     }
 
     // Update is called once per frame
@@ -20,9 +30,29 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal"); //we use GetAxisRaw to directly change the value, (no acceleration). If we used GetAxis then the character would accelerate while moving
         change.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetButtonDown("Attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk){
+
+        
         UpdateAnimation();
+        }
 
     }
+
+     private IEnumerator AttackCo()
+     {
+        animator.SetBool("attacking",true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking",false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+        
+     }
+
     void UpdateAnimation()
     { MoveCharacter();
          if(change != Vector3.zero)
@@ -41,7 +71,7 @@ public class PlayerMovement : MonoBehaviour
 
     void MoveCharacter()
     {   
-     
+            change.Normalize();
        // myRigidBody.MovePosition(transform.position + change*Speed() *Time.deltaTime);
        myRigidBody.velocity = new Vector2(change.x*Speed(),change.y*Speed());
        
